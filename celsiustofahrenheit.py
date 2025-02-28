44ac1b6e663bf6f53ef1c00500b99335479c3b5e
Programming-King100 <166244368+Programming-King100@users.noreply.github.com>
2025-02-28 17:47:14 +0100

    Add files via upload

--- celsiustofahrenheit.py
+++ celsiustofahrenheit.py
@@ -6,12 +6,19 @@ root.title("Uchana weather Magic")
 
 
 CandF=Label(root,text="Celsius -> Fahrenheit",font=font.Font(size=20),fg="Purple")
-CandF.grid(row=4,column=4,columnspan=9)   
-Alachbutton=Button(root,text="Translate")
+CandF.grid(row=4,column=4,columnspan=9)
+
+def convert():
+    temp_celcius=jaeb.get()
+    if temp_celcius.replace('.','').isdigit():
+        temp_fah=(float(temp_celcius)*9/5)+32
+        mT.config(text=''+str(temp_fah)+'°F')
+   
+Alachbutton=Button(root,text="Translate",command=convert)
 Alachbutton.grid(row=7,column=4,columnspan=9)
 
 tctf=Label(root,text="Translate Celsius to Fahrenheit:")
-tctf.grid(row=5,column=4,co)
+tctf.grid(row=5,column=4,)
 
 jaeb = Entry(root,width=30)
 jaeb.grid(row=5,column=5)
